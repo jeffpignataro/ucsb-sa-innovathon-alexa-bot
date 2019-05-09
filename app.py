8234b90685425070c9b6ac624e12cda1a6692714
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-05-09 11:10:44 -0700

    First attempt at Alexa response

--- app.py
+++ app.py
@@ -2,7 +2,24 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    return {
+    QUESTION = 'test'
+    responseBody = buildAlexaJson(QUESTION)
+    returnObj =  {
         'statusCode': 200,
-        'body': json.dumps('Hello Innovathon from Lambda!')
+        'body': json.dumps(responseBody)
     }
+    print(returnObj)
+    return returnObj
+
+def buildAlexaJson(question):
+    RETURNJSON = {}
+    RETURNJSON["body"] = {}
+    RETURNJSON["body"]["version"] = '1.0'
+    RETURNJSON["body"]["response"] = {}
+    RETURNJSON["body"]["response"]["outputSpeech"] = {}
+    RETURNJSON["body"]["response"]["outputSpeech"]["type"] = "SSML"
+    RETURNJSON["body"]["response"]["outputSpeech"]["ssml"] = "<speak>Hi Eric!</speak>"
+    RETURNJSON["body"]["response"]["type"] = "_DEFAULT_RESPONSE"
+    RETURNJSON["body"]["sessionAttributes"] = {}
+    RETURNJSON["body"]["userAgent"] = 'ask-node/2.3.0 Node/v8.10.0'
+    return RETURNJSON
--- tests.py
+++ tests.py
@@ -1,1 +1,5 @@
+from app import lambda_handler
+
+lambda_handler(None, None)
+
 print("Stub for deployment tests")
